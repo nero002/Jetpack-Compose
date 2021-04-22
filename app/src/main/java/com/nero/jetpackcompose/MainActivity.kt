59bdfa1778094670e3590c124556d7da67166b38
Dheeraj Gupta <38027375+nero002@users.noreply.github.com>
2021-04-22 23:10:58 +0530

    hello world

---
+++ app/src/main/java/com/nero/jetpackcompose/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.nero.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.nero.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+           Greeting(name = "Dheeraj")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeTheme {
+        Greeting("Android")
+    }
+}
